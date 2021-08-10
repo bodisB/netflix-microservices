4bc548878baa22a204209bb0e3e9c67621a5919c
bodisB <bencebodis07@gmail.com>
2021-08-10 13:26:04 +0200

    feat: initialize spring project

---
+++ src/main/java/com/codecool/netflixapp/NetflixAppApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.netflixapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflixAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetflixAppApplication.class, args);
+    }
+
+}
