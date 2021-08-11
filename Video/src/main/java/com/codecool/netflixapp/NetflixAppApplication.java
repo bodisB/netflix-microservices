d17a5794b81093c27c674db4b94e2a46060cd5cb
bodisB <bencebodis07@gmail.com>
2021-08-11 13:05:19 +0200

    feat: added Eureka, Docket and RestTemplate to the project

--- Video/src/main/java/com/codecool/netflixapp/NetflixAppApplication.java
+++ Video/src/main/java/com/codecool/netflixapp/NetflixAppApplication.java
@@ -5,11 +5,21 @@ import com.codecool.netflixapp.repository.VideoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import javax.annotation.PostConstruct;
 import java.util.Arrays;
 
 @SpringBootApplication
+@EnableSwagger2
+@EnableEurekaClient
 public class NetflixAppApplication {
 
     @Autowired
@@ -18,6 +28,21 @@ public class NetflixAppApplication {
     public static void main(String[] args) {
         SpringApplication.run(NetflixAppApplication.class, args);
     }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.ant("/video/**"))
+                .build();
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
     @PostConstruct
     public void setup() {
         Video whatever = Video.builder()
